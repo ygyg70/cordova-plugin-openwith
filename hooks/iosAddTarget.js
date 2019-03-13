9100d31543797c2372f642f3d801e5a7702930de
Etienne Lemay <etienne@heliom.ca>
2019-03-13 10:30:36 -0400

    DRY / cleanup hook scripts

--- hooks/iosAddTarget.js
+++ hooks/iosAddTarget.js
@@ -1,120 +1,36 @@
-//
-//  iosAddTarget.js
-//  This hook runs for the iOS platform when the plugin or platform is added.
-//
-// Source: https://github.com/DavidStrausz/cordova-plugin-today-widget
-//
+const fs = require('fs');
+const path = require('path');
 
-//
-// The MIT License (MIT)
-//
-// Copyright (c) 2017 DavidStrausz
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-//
+const {
+  PLUGIN_ID,
+  iosFolder,
+  getPreferences,
+  findXCodeproject,
+  replacePreferencesInFile,
+  log, redError,
+} = require('./utils')
 
-const PLUGIN_ID = 'cordova-plugin-openwith';
-const BUNDLE_SUFFIX = '.shareextension';
-
-var fs = require('fs');
-var path = require('path');
-
-function redError(message) {
-    return new Error('"' + PLUGIN_ID + '" \x1b[1m\x1b[31m' + message + '\x1b[0m');
-}
-
-function replacePreferencesInFile(filePath, preferences) {
-    var content = fs.readFileSync(filePath, 'utf8');
-    for (var i = 0; i < preferences.length; i++) {
-        var pref = preferences[i];
-        var regexp = new RegExp(pref.key, "g");
-        content = content.replace(regexp, pref.value);
-    }
-    fs.writeFileSync(filePath, content);
-}
-
-// Determine the full path to the app's xcode project file.
-function findXCodeproject(context, callback) {
-  fs.readdir(iosFolder(context), function(err, data) {
-    var projectFolder;
-    var projectName;
-    // Find the project folder by looking for *.xcodeproj
-    if (data && data.length) {
-      data.forEach(function(folder) {
-        if (folder.match(/\.xcodeproj$/)) {
-          projectFolder = path.join(iosFolder(context), folder);
-          projectName = path.basename(folder, '.xcodeproj');
-        }
-      });
-    }
-
-    if (!projectFolder || !projectName) {
-      throw redError('Could not find an .xcodeproj folder in: ' + iosFolder(context));
-    }
-
-    if (err) {
-      throw redError(err);
-    }
-
-    callback(projectFolder, projectName);
-  });
-}
-
-// Determine the full path to the ios platform
-function iosFolder(context) {
-  return context.opts.cordova.project
-    ? context.opts.cordova.project.root
-    : path.join(context.opts.projectRoot, 'platforms/ios/');
-}
-
-function getPreferenceValue(configXml, name) {
-  var value = configXml.match(new RegExp('name="' + name + '" value="(.*?)"', "i"));
-  if (value && value[1]) {
-    return value[1];
-  } else {
-    return null;
-  }
-}
-
-function getCordovaParameter(configXml, variableName) {
-  var variable;
-  var arg = process.argv.filter(function(arg) {
-    return arg.indexOf(variableName + '=') == 0;
-  });
-  if (arg.length >= 1) {
-    variable = arg[0].split('=')[1];
-  } else {
-    variable = getPreferenceValue(configXml, variableName);
-  }
-  return variable;
-}
+// Return the list of files in the share extension project, organized by type
+const FILE_TYPES = {
+  '.h':'source',
+  '.m':'source',
+  '.plist':'config',
+  '.entitlements':'config',
+};
 
 function parsePbxProject(context, pbxProjectPath) {
   var xcode = context.requireCordovaModule('xcode');
-  console.log('    Parsing existing project at location: ' + pbxProjectPath + '...');
+  log(`Parsing existing project at location: ${pbxProjectPath}…`);
+
   var pbxProject;
+
   if (context.opts.cordova.project) {
     pbxProject = context.opts.cordova.project.parseProjectFile(context.opts.projectRoot).xcode;
   } else {
     pbxProject = xcode.project(pbxProjectPath);
     pbxProject.parseSync();
   }
+
   return pbxProject;
 }
 
@@ -124,116 +40,45 @@ function forEachShareExtensionFile(context, callback) {
     // Ignore junk files like .DS_Store
     if (!/^\..*/.test(name)) {
       callback({
-        name:name,
-        path:path.join(shareExtensionFolder, name),
-        extension:path.extname(name)
+        name: name,
+        path: path.join(shareExtensionFolder, name),
+        extension: path.extname(name)
       });
     }
   });
 }
 
-function projectPlistPath(context, projectName) {
-  return path.join(iosFolder(context), projectName, projectName + '-Info.plist');
-}
-
-function projectPlistJson(context, projectName) {
-  var plist = require('plist');
-  var path = projectPlistPath(context, projectName);
-  return plist.parse(fs.readFileSync(path, 'utf8'));
-}
-
-function getPreferences(context, configXml, projectName) {
-  var plist = projectPlistJson(context, projectName);
-  return [{
-    key: '__DISPLAY_NAME__',
-    value: getCordovaParameter(configXml, 'DISPLAY_NAME') || projectName
-  }, {
-    key: '__BUNDLE_IDENTIFIER__',
-    value: getCordovaParameter(configXml, 'IOS_BUNDLE_IDENTIFIER') + BUNDLE_SUFFIX
-  }, {
-    key: '__BUNDLE_SHORT_VERSION_STRING__',
-    value: plist.CFBundleShortVersionString
-  }, {
-    key: '__BUNDLE_VERSION__',
-    value: plist.CFBundleVersion
-  }, {
-    key: '__URL_SCHEME__',
-    value: getCordovaParameter(configXml, 'IOS_URL_SCHEME')
-  }];
-}
-
-// Return the list of files in the share extension project, organized by type
-var FILE_TYPES = {
-  '.h':'source',
-  '.m':'source',
-  '.plist':'config',
-  '.entitlements':'config',
-};
-
 function getShareExtensionFiles(context) {
   var files = { source: [], config: [], resource: [] };
+
   forEachShareExtensionFile(context, function(file) {
     var fileType = FILE_TYPES[file.extension] || 'resource';
     files[fileType].push(file);
   });
+
   return files;
 }
 
-function printShareExtensionFiles(files) {
-  console.log('    Found following files in your ShareExtension folder:');
-  console.log('    Source files:');
-  files.source.forEach(function(file) {
-    console.log('     - ', file.name);
-  });
-
-  console.log('    Config files:');
-  files.config.forEach(function(file) {
-    console.log('     - ', file.name);
-  });
-
-  console.log('    Resource files:');
-  files.resource.forEach(function(file) {
-    console.log('     - ', file.name);
-  });
-}
-
-console.log('Adding target "' + PLUGIN_ID + '/ShareExtension" to XCode project');
-
-module.exports = function (context) {
+module.exports = function(context) {
+  log('Adding ShareExt target to XCode project')
 
   var Q = context.requireCordovaModule('q');
   var deferral = new Q.defer();
 
-  if (context.opts.cordova.platforms.indexOf('ios') < 0) {
-    log('You have to add the ios platform before adding this plugin!', 'error');
-  }
-
-  var configXml = fs.readFileSync(path.join(context.opts.projectRoot, 'config.xml'), 'utf-8');
-  if (configXml) {
-    configXml = configXml.substring(configXml.indexOf('<'));
-  }
-
   findXCodeproject(context, function(projectFolder, projectName) {
-
-    console.log('  - Folder containing your iOS project: ' + iosFolder(context));
+    var preferences = getPreferences(context, projectName);
 
     var pbxProjectPath = path.join(projectFolder, 'project.pbxproj');
     var pbxProject = parsePbxProject(context, pbxProjectPath);
 
     var files = getShareExtensionFiles(context);
-    // printShareExtensionFiles(files);
-
-    var preferences = getPreferences(context, configXml, projectName);
     files.config.concat(files.source).forEach(function(file) {
       replacePreferencesInFile(file.path, preferences);
-      // console.log('    Successfully updated ' + file.name);
     });
 
     // Find if the project already contains the target and group
     var target = pbxProject.pbxTargetByName('ShareExt');
-    if (target) {
-      console.log('    ShareExt target already exists.');
-    }
+    if (target) { log('ShareExt target already exists') }
 
     if (!target) {
       // Add PBXNativeTarget to the project
@@ -250,9 +95,8 @@ module.exports = function (context) {
 
     // Create a separate PBXGroup for the shareExtensions files, name has to be unique and path must be in quotation marks
     var pbxGroupKey = pbxProject.findPBXGroupKey({name: 'ShareExtension'});
-    if (pbxProject) {
-      console.log('    ShareExtension group already exists.');
-    }
+    if (pbxProject) { log('ShareExtension group already exists') }
+
     if (!pbxGroupKey) {
       pbxGroupKey = pbxProject.pbxCreateGroup('ShareExtension', 'ShareExtension');
 
@@ -292,7 +136,7 @@ module.exports = function (context) {
 
     // Write the modified project back to disc
     fs.writeFileSync(pbxProjectPath, pbxProject.writeSync());
-    console.log('Added ShareExtension to XCode project');
+    log('Successfully added ShareExt target to XCode project')
 
     deferral.resolve();
   });

44cc63ed0a73250dde9d8543f1bdc8c3bde18089
Etienne Lemay <etienne@heliom.ca>
2019-03-15 15:36:08 -0400

    Send image file path to extension instead of base64 Like we do for videos

--- src/ios/ShareExtension/ShareViewController.m
+++ src/ios/ShareExtension/ShareViewController.m
@@ -3,51 +3,6 @@
 #import "ShareViewController.h"
 #import <MobileCoreServices/MobileCoreServices.h>
 
-/*
- * Add base64 export to NSData
- */
-@interface NSData (Base64)
-- (NSString*) convertToBase64;
-@end
-
-@implementation NSData (Base64)
-- (NSString*) convertToBase64 {
-  const uint8_t* input = (const uint8_t*)[self bytes];
-  NSInteger length = [self length];
-
-  static char table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-
-  NSMutableData* data = [NSMutableData dataWithLength:((length + 2) / 3) * 4];
-  uint8_t* output = (uint8_t*)data.mutableBytes;
-
-  NSInteger i;
-  for (i=0; i < length; i += 3) {
-    NSInteger value = 0;
-    NSInteger j;
-
-    for (j = i; j < (i + 3); j++) {
-      value <<= 8;
-
-      if (j < length) {
-        value |= (0xFF & input[j]);
-      }
-    }
-
-    NSInteger theIndex = (i / 3) * 4;
-    output[theIndex + 0] =                    table[(value >> 18) & 0x3F];
-    output[theIndex + 1] =                    table[(value >> 12) & 0x3F];
-    output[theIndex + 2] = (i + 1) < length ? table[(value >> 6)  & 0x3F] : '=';
-    output[theIndex + 3] = (i + 2) < length ? table[(value >> 0)  & 0x3F] : '=';
-  }
-
-  NSString *ret = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
-#if ARC_DISABLED
-  [ret autorelease];
-#endif
-  return ret;
-}
-@end
-
 @interface ShareViewController : SLComposeServiceViewController <UIAlertViewDelegate> {
   NSFileManager *_fileManager;
   NSUserDefaults *_userDefaults;
@@ -194,8 +149,7 @@
       lastDataType = [NSString stringWithFormat:@"MOVIE_OR_IMAGE"];
 
       [itemProvider loadItemForTypeIdentifier:@"public.image" options:nil completionHandler: ^(NSURL* item, NSError *error) {
-        NSData *data = [NSData dataWithContentsOfURL:(NSURL*)item];
-        NSString *base64 = [data convertToBase64];
+        NSString *fileUrl = [self saveFileToAppGroupFolder:item];
         NSString *suggestedName = item.lastPathComponent;
 
         NSString *uti = @"public.image";
@@ -210,7 +164,7 @@
         NSString *mimeType =  [self mimeTypeFromUti:registeredType];
         NSDictionary *dict = @{
           @"text" : self.contentText,
-          @"data" : base64,
+          @"fileUrl" : fileUrl,
           @"uti"  : uti,
           @"utis" : itemProvider.registeredTypeIdentifiers,
           @"name" : suggestedName,
